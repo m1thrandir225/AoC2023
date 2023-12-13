c2ff06ccce6e1f1e4ba0679e82f666c639089dfc
Sebastijan Zindl <sebastijan32@hotmail.com>
2023-12-13 19:52:29 +0100

    feat: added boilerplate to day 6

---
+++ Sources/Day06.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sebastijan Zindl on 13.12.23.
+//
+
+import Foundation
+
+struct Day06: AdventDay {
+    let data: String;
+    
+    func part1() async throws -> Int {
+        return 0;
+    }
+    func part2() async throws -> Int {
+        return 0;
+    }
+}
