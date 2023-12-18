a06d6c2696f59df7bfa37e780d1386515869b37a
Sebastijan Zindl <sebastijan32@hotmail.com>
2023-12-18 16:11:43 +0100

    feat: started day 7

---
+++ Sources/Day07.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sebastijan Zindl on 18.12.23.
+//
+
+import Foundation
+
+
+struct Day07: AdventDay {
+    let data: String;
+    
+    func part1() throws -> Int {
+        return 0;
+    }
+    
+    func part2() throws -> Int {
+        return 0;
+    }
+}
---
+++ Tests/Day07.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sebastijan Zindl on 18.12.23.
+//
+
+import XCTest
+
+@testable import AdventOfCode
+
+final class Day07Tests: XCTestCase {
+    let testData = """
+    32T3K 765
+    T55J5 684
+    KK677 28
+    KTJJT 220
+    QQQJA 483
+    """
+    
+    func testPart1() {
+        
+    }
+    
+    func testPart2() {}
+}
