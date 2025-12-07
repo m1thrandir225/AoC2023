47ffbb414556b7c7a694acc51ad0f74cb5bafd66
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-12-07 13:24:44 +0100

    feat: lastover uncommited stuff

---
+++ Sources/Day08.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sebastijan Zindl on 25.12.23.
+//
+
+import Foundation
---
+++ Tests/Day08.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sebastijan Zindl on 25.12.23.
+//
+
+import XCTest
+
+@testable import AdventOfCode
+
+final class Day08Tests: XCTestCase {
+    let testData = """
+    
+    """
+    
+    func testPart1() {
+        
+    }
+    
+    func testPart2() {
+        
+    }
+}
